622bebdc46a34f162f0e82388f53f9127a99fe2f
elantsev <elantsev.pa@gmail.com>
2020-09-20 22:34:50 +0300

    remove

--- src/pages/Film.vue
+++ src/pages/Film.vue
@@ -1,15 +1,4 @@
 <template>
-  <!-- <div class="film">
-    <router-link append :to="filmId">
-      <h5 class="film__title"><b>title:</b> {{ f.title }}</h5>
-    </router-link>
-    <p class="film__field"><b>director:</b> {{ f.director }}</p>
-    <p class="film__field"><b>producer:</b> {{ f.producer }}</p>
-    <p class="film__field"><b>release date:</b> {{ f.release_date }}</p>
-    <p class="film__field"><b>opening crawl: </b> {{ f.opening_crawl }}</p>
-    <p class="film__field"><b>created: </b> {{ f.created | formatDate }}</p>
-    <p class="film__field"><b>edited:</b> {{ f.edited | formatDate }}</p>
-  </div> -->
   <div class="film">
     <h4 class="film__header">Film</h4>
     <c-preloader v-if="loading"></c-preloader>
