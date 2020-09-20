270b62e07d478c4755dd80951721238bfa9960e7
elantsev <elantsev.pa@gmail.com>
2020-09-20 20:58:57 +0300

    fix variable name

--- src/store/films/mutations.js
+++ src/store/films/mutations.js
@@ -1,6 +1,6 @@
 export function setFilms (state, films) {
     state.films = films
-    const filmsHash = people.reduce((acc, film) => { return { ...acc, [film.url]: film } }, {})
+    const filmsHash = films.reduce((acc, film) => { return { ...acc, [film.url]: film } }, {})
     state.filmsHash = { ...state.filmsHash, ...filmsHash }
 }
 export function setFilm (state, film) {
